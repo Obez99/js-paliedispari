15c33b4f6007d8835afe8926a8cd0f4df912cde1
Obez99 <giuliogx@live.it>
2021-10-18 17:20:01 +0200

    Creates random number for ai,  sums player and ai numbers

--- Pari e Dispari/scripts.js
+++ Pari e Dispari/scripts.js
@@ -1,4 +1,11 @@
+//Generare un numero random da 1 a 5 per il computer con una funzione
+function aiRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
+  return randomNumber;
+}
+
 //Far scegliere all'utente pari o dispari
+let userNumber = 0;
 let userEvenOdd = prompt("Pari o dispari?");
 userEvenOdd = userEvenOdd.toLowerCase();
 console.log(userEvenOdd);
@@ -8,13 +15,19 @@ if (!isNaN || userEvenOdd != "pari" && userEvenOdd != "dispari") {
 }
 else {
   //Far scegliere all'utente un numero
-  let userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+  userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
   if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
     alert("Inserisci un numero valido.")
     location.reload();
   }
 }
+
 //Generare un numero random da 1 a 5 per il computer con una funzione
+const aiNumber = aiRandomNumber();
+console.log(aiNumber);
+
 //Sommare i numeri del computer e del giocatore
+const sum = userNumber + aiNumber;
+console.log(sum);
 //Stabilire se la somma dei due numeri è pari o dispari con un funzione
 //Dichiarare il vincitore
