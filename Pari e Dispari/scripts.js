fe23689eac02f40c048b19294e6bdf1cf4307bc9
Obez99 <giuliogx@live.it>
2021-10-18 17:05:29 +0200

    Asks user to choose a number

--- Pari e Dispari/scripts.js
+++ Pari e Dispari/scripts.js
@@ -3,9 +3,17 @@ let userEvenOdd = prompt("Pari o dispari?");
 userEvenOdd = userEvenOdd.toLowerCase();
 console.log(userEvenOdd);
 if (!isNaN || userEvenOdd != "pari" && userEvenOdd != "dispari") {
-  alert("Inserisci un valore corretto")
+  alert("Inserisci un valore corretto");
+  location.reload();
 }
-//Far scegliere all'utente un numero
+else {
+  //Far scegliere all'utente un numero
+  let userNumber = parseInt(prompt("Scegli un numero da 1 a 5"));
+  if (isNaN(userNumber) || userNumber < 1 || userNumber > 5) {
+    alert("Inserisci un numero valido.")
+    location.reload();
+  }
+}
 //Generare un numero random da 1 a 5 per il computer con una funzione
 //Sommare i numeri del computer e del giocatore
 //Stabilire se la somma dei due numeri è pari o dispari con un funzione
