3ac4733bfb42410ad51f7ccca4bdb3ad7410c7c4
Obez99 <giuliogx@live.it>
2021-10-18 17:22:26 +0200

    Creates new js file for functions

--- Pari e Dispari/js/scripts.js
+++ Pari e Dispari/js/scripts.js
@@ -1,3 +1,8 @@
+//Generare un numero random da 1 a 5 per il computer con una funzione
+function aiRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
+  return randomNumber;
+}
 
 //Far scegliere all'utente pari o dispari
 let userNumber = 0;
---
+++ Pari e Dispari/js/utilities.js
@@ -0,0 +1,5 @@
+//Generare un numero random da 1 a 5 per il computer con una funzione
+function aiRandomNumber() {
+  const randomNumber = Math.floor(Math.random() * 5) + 1;
+  return randomNumber;
+}
