423cc9f79d136900880b5f131656bd051cc3de3e
Obez99 <giuliogx@live.it>
2021-10-18 15:52:19 +0200

    Inverts array order

--- Palindroma/scripts.js
+++ Palindroma/scripts.js
@@ -1,6 +1,16 @@
 //Chiedere all'utente di inserire una parola
+const word = prompt("Inserisci una parola");
+let letters = [];
 
 //Scomporre la parola in un array lettera per lettera
+for (let i = 0; i < word.length; i++) {
+  letters[i] = word[i];
+}
+
+//Invertire l'ordine dell'array
+for (let i = word.length - 1; i >= 0; i--) {
+  console.log(letters[i]);
+}
 
 //Convertire l'array in una stringa
 
