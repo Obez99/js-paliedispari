ef6b4a4a231a42194d25ff40ef206a4dff758efc
Obez99 <giuliogx@live.it>
2021-10-18 14:52:31 +0200

    Creates base files for "Palindroma" exercise

---
+++ Palindroma/scripts.js
@@ -0,0 +1,7 @@
+//Chiedere all'utente di inserire una parola
+
+//Scomporre la parola in un array lettera per lettera
+
+//Convertire l'array in una stringa
+
+//Confrontare la stringa finale e confrontarla con quella inserita dall'utente
