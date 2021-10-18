e3253ec6e366d6cfff20b2dc7311270a60a98f73
Obez99 <giuliogx@live.it>
2021-10-18 16:04:13 +0200

    Converts array into a string

--- Palindroma/scripts.js
+++ Palindroma/scripts.js
@@ -8,10 +8,14 @@ for (let i = 0; i < word.length; i++) {
 }
 
 //Invertire l'ordine dell'array
+let reverseWord = [];
+let reverseIndex = 0;
 for (let i = word.length - 1; i >= 0; i--) {
-  console.log(letters[i]);
+  reverseWord[reverseIndex] = letters[i];
+  reverseIndex++;
 }
 
 //Convertire l'array in una stringa
-
+reverseWord = reverseWord.join("");
+console.log(reverseWord);
 //Confrontare la stringa finale e confrontarla con quella inserita dall'utente
