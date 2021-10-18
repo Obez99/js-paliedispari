832d3533b3bac1a65e7a4f2e752e3849ad3c2d7b
Obez99 <giuliogx@live.it>
2021-10-18 16:20:12 +0200

    Completes first exercise

--- Palindroma/scripts.js
+++ Palindroma/scripts.js
@@ -1,3 +1,29 @@
+function palindromeCheck() {
+  //Scomporre la parola in un array lettera per lettera
+  let letters = [];
+  for (let i = 0; i < word.length; i++) {
+    letters[i] = word[i];
+  }
+
+  //Invertire l'ordine dell'array
+  let reverseWord = [];
+  let reverseIndex = 0;
+  for (let i = word.length - 1; i >= 0; i--) {
+    reverseWord[reverseIndex] = letters[i];
+    reverseIndex++;
+  }
+
+  //Convertire l'array in una stringa
+  reverseWord = reverseWord.join("");
+
+  //Confrontare la stringa finale e confrontarla con quella inserita dall'utente
+  if (reverseWord.toLowerCase() === word.toLowerCase()) {
+    return alert("La parola è palindroma");
+  } else {
+    return alert("La parola non è palindroma");
+  }
+}
+
 //Chiedere all'utente di inserire una parola e controllare che lo sia
 const word = prompt("Inserisci una parola");
 if (!isNaN(word)) {
@@ -5,26 +31,5 @@ if (!isNaN(word)) {
   location.reload();
 }
 
-//Scomporre la parola in un array lettera per lettera
-let letters = [];
-for (let i = 0; i < word.length; i++) {
-  letters[i] = word[i];
-}
+palindromeCheck(word)
 
-//Invertire l'ordine dell'array
-let reverseWord = [];
-let reverseIndex = 0;
-for (let i = word.length - 1; i >= 0; i--) {
-  reverseWord[reverseIndex] = letters[i];
-  reverseIndex++;
-}
-
-//Convertire l'array in una stringa
-reverseWord = reverseWord.join("");
-
-//Confrontare la stringa finale e confrontarla con quella inserita dall'utente
-if (reverseWord.toLowerCase() === word.toLowerCase()) {
-  alert("La parola è palindroma")
-} else {
-  alert("La parola non è palindroma")
-}
